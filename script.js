c32cd4443ff087fa3383d121e6c2da14fbb1e2b3
AksharPrasanna <aksharprasanna@gmail.com>
2024-03-06 22:13:09 +0530

    First Assignment

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 let js = 'amazing';
 if (js === 'amazing') alert('Javascript is FUN');
 console.log(40 + 8 + 23 - 10);
+
+let firstName = 'Jonas';
+console.log(firstName);
+
+let country = 'India';
+let continent = 'Asia';
+let population = '1.5B';
+
+console.log(country + ' is situated in ' + continent + ' and has a population of ' + population);   
